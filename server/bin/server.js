8ad93e12c5dbaa97925a9a470a34e0ab24604b3e
Kirill Bogdanov <kiralbogdanov@gmail.com>
2020-11-28 14:09:02 +0300

    heroku fix 3

--- server/bin/server.js
+++ server/bin/server.js
@@ -3,9 +3,10 @@
 import createApp from '../index.js';
 
 const port = process.env.PORT || 8080;
+const address = '0.0.0.0';
 const app = createApp();
 
-app.listen(port, (err) => {
+app.listen(port, address, (err) => {
   if (err) {
     app.log.error(err);
     process.exit(1);
